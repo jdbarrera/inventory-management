1f69f6ff4adcc1463732844f160aaac79f8f9c00
Jason Barrera <jasonbarrera@Mac.attlocal.net>
2025-02-11 20:02:16 -0600

    add dashboard route and controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
+app.use("/dashboard", dashboardRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3001;
