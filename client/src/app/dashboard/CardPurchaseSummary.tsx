b923a370626bb48c6ebdd81bab6bdf6fcefbf812
Jason Barrera <jasonbarrera@Mac.attlocal.net>
2025-03-05 15:47:27 -0600

    add CardExpenseSummary

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -18,7 +18,7 @@ const CardPurchaseSummary = () => {
   const lastDataPoint = purchaseData[purchaseData.length - 1] || null;
 
   return (
-    <div className="flex flex-col justify-between shadow-md rounded-2xl row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white">
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
       {isLoading ? (
         <div className="m-5">Loading...</div>
       ) : (
@@ -61,21 +61,29 @@ const CardPurchaseSummary = () => {
               </div>
             </div>
             {/* CHART */}
-            <ResponsiveContainer width="100%" height={350} className="px-7">
+            <ResponsiveContainer width="100%" height={200} className="p-2">
               <AreaChart
                 data={purchaseData}
                 margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
               >
                 <XAxis dataKey="date" tick={false} axisLine={false} />
-                <YAxis tick={false} tickLine={false} axisLine={false} />
+                <YAxis tickLine={false} tick={false} axisLine={false} />
                 <Tooltip
                   formatter={(value: number) => [
                     `$${value.toLocaleString("en")}`,
                   ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
                 />
                 <Area
                   type="linear"
-                  dataKey={"totalPurchased"}
+                  dataKey="totalPurchased"
                   stroke="#8884d8"
                   fill="#8884d8"
                   dot={true}
