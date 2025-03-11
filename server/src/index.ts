1ecb15ca5a5e5f794f0f80d94914b60ebcce6f21
Jason Barrera <jasonbarrera@Mac.attlocal.net>
2025-03-10 19:53:22 -0500

    add expense BE logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,9 @@ import morgan from "morgan";
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
+import expenseRoutes from "./routes/expenseRoutes";
 
-/* CONFIGURATION */
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -21,12 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.use("/dashboard", dashboardRoutes);
-app.use("/products", productRoutes);
-app.use("/users", userRoutes);
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
+app.use("/users", userRoutes); // http://localhost:8000/users
+app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
 
 /* SERVER */
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
